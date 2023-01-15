affdbd5c5fba40c6e40de69ce9d65106441dd04f
BIBI78 <blakecollins78@gmail.com>
2023-01-15 14:35:23 +0000

    add function for weight lossclear

--- run.py
+++ run.py
@@ -2,23 +2,6 @@ import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
-"""
-def weight_loss_time(weight, desired_weight, deficit_per_day):
-    weight_loss_rate = weight - desired_weight
-    #this eqn is from the net
-    deficit_needed = weight_loss_rate * 7700
-    days_needed = deficit_needed / deficit_per_day
-    return days_needed
-
-
-
-weight = float(input("Enter your current weight in kg: "))
-desired_weight = float(input("Enter your desired weight in kg: "))
-deficit_per_day = int(input("Enter your estimated calorie deficit per day/ how much you plan to eat per day: "))
-time = weight_loss_time(weight, desired_weight, deficit_per_day)
-print(f"It will take approximately {time} days to reach your desired weight")  
-
-"""
 
 
 def weight_loss_time(weight, desired_weight,age,height):
@@ -34,5 +17,4 @@ desired_weight = float(input("Enter your desired weight in kg: "))
 recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
 deficit_per_day = recomended_deficit
 time = weight_loss_time(weight, desired_weight,age,height)
-#print(f"It will take qbout {time} days to reach your desired weight")  
 print(f"You should eat about  {recomended_deficit} calories per day for about {time} days") 
