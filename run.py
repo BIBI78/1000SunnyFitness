8c0cf6e784422bd5dcf23a1b58d1b76d118d3e61
BIBI78 <blakecollins78@gmail.com>
2023-01-16 11:22:23 +0000

    add sketch for update spread sheet function

--- run.py
+++ run.py
@@ -121,23 +121,6 @@ def weight_gain_time(weight, height, age, desired_weight):
 
 
 
-sales = SHEET.worksheet('Sheet1')
-data = sales.get_all_values()
-
-
-print(data)
-
-
-
-
-
-
-
-
-
-
-
-
 
 """
 def main():
@@ -174,3 +157,104 @@ print("and will take approximately {} weeks to reach your desired weight.".forma
 """
 
 
+"""
+
+sales = SHEET.worksheet('Sheet1')
+data = sales.get_all_values()
+
+
+print(data)
+
+"""
+"""
+def get_user_data():
+    print ("please answer the following questions")
+    weight = float(input("Enter your current weight (in kg): "))
+    height = float(input("Enter your height (in cm): "))
+    age = int(input("Enter your age: "))
+    desired_weight = float(input("Enter your desired weight (in kg): "))
+
+
+
+"""
+
+
+
+#get_user_data()
+
+"""
+
+def update_data_sheet(data):
+
+    print("updating worksheet")
+    user_worksheet = SHEET.worksheet("Sheet1")
+    user_worksheet.append_row(data)
+
+
+
+
+data= get_user_data()
+user_data = data
+update_data_sheet(data)
+
+
+update_data_sheet(data)
+"""
+
+
+
+def get_user_data():
+    """
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+
+        data_str = input("Enter your data here: ")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("Sheet1")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
