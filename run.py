d30d92d51d4d220fe00aa46a2345576ff6057524
BIBI78 <blakecollins78@gmail.com>
2023-04-11 16:58:39 +0000

    add update to weight loss/gain function, add user info fucntion

--- run.py
+++ run.py
@@ -44,36 +44,84 @@ def draw_jolly_roger():
 
 #WEIGHT FUNCTIONS
 # Weight loss function , this calculates the days needed for the user to lose weight.
-def weight_loss_time(weight, desired_weight,age,height):
-    weight_loss_rate = weight - desired_weight
-    deficit_needed = weight_loss_rate * 7700
-    recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
-    deficit_per_day = recomended_deficit 
-    days_needed = deficit_needed / deficit_per_day
-    recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
-    deficit_needed = recomended_deficit
-    return days_needed 
+# So this new function combines weight loss and and weight gain. 
+# this function takes the user data provided earlier and does  the necessary calculations for weight loss or gain
+def weight_change(user_info):
+    weight_change = user_info["weight_change"]
+    while True:
+        try:
+            weight = float(input("Enter your current weight in kg: "))
+            if weight <= 0:
+                raise ValueError("Nobody is that light,weeight must be greater than 0.")
+            break
+        except ValueError as e:
+            print(f"We need a number, try again.")
 
-#Weight gain function , this calculates the days needed for the user to gain weight.
-def weight_gain_time(weight, height, age, desired_weight):
-    # this is the bmr for weight loss 
-    BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
-    weight_gain_time = BMR * 1.55
-    time_to_reach_desired_weight = (desired_weight - weight) / 0.5
-    return weight_gain_time, time_to_reach_desired_weight
+    while True:
+        try:
+            height = float(input("Enter your height (in cm): "))
+            if height <= 0:
+                raise ValueError("Height must be greater than 0.")
+            break
+        except ValueError as e:
+            print(f"Huh? Height must be greater than 0.")
 
-#
+    age = int(user_info["age"])
+
+    while True:
+        try:
+            desired_weight = float(input("Enter your desired weight in kg: ")) if weight_change == "lose" else float(input("Enter the weight you'd like to build up to (in kg): "))
+            if desired_weight <= 0:
+                raise ValueError("Desired weight must be greater than 0.")
+            if weight_change == "gain" and desired_weight <= weight:
+                raise ValueError("Desired weight must be greater than current weight.")
+            break
+        except ValueError as e:
+            print(f"Invalid input, please enter a number")
+
+    if weight_change == "lose":
+        weight_loss_rate = weight - desired_weight
+        deficit_needed = weight_loss_rate * 7700
+        recomended_deficit = (10*(weight) + 6.25*(height) -(5*age)+5)
+        days_needed = deficit_needed / recomended_deficit
+        print(f"You should eat about {recomended_deficit} calories per day for about {days_needed} days")
+        user_data = (weight, height, age, desired_weight)
+    else:
+        BMR = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+        calories_per_day = BMR * 1.55
+        time_to_reach_desired_weight = (desired_weight - weight) / 0.5
+        print("To gain this weight, you should eat about", ceil(calories_per_day), "calories per day.")
+        print(f"and it will take approximately {time_to_reach_desired_weight} weeks to reach your desired weight.")
+        user_data = (weight, height, age, desired_weight)
+
+    return user_data
 
 
 
 
-
-
-
-
+#USER INFO
+def get_user_info():
+    print("WELCOME TO 1000 SUNNY FITNESS \n")
+    name = input("What's your name?\n")
+    while True:
+        age = input("How old are you?\n")
+        if age.isnumeric() and int(age) > 0:
+            break
+        print("Sorry matey lets get your age again.")
+    # do i even need this really ?
+    gender = input("What gender do you identify as?\n") 
+    while True:
+        weight_change = input("Would you like to lose or gain weight? (lose or gain): ")
+        if weight_change == "lose" or weight_change == "gain":
+            break
+        print("That doesnt work, please enter either 'lose' or 'gain'")
+    user_info = {"name": name, "age": age, "gender": gender, "weight_change": weight_change}
+    return user_info
+  
 
 def main():
     draw_jolly_roger()
-    
+    user_info = get_user_info()
+    weight_change(user_info)
 
 main()
