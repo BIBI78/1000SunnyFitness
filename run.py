52b11560d4dd7db76dedfacdcff3acdadf29bf29
BIBI78 <blakecollins78@gmail.com>
2023-04-11 12:58:29 +0000

    add pirate flag design

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import requests
 import json
 import random
 import time 
-from pprint import pprint
 from math import ceil
 
 
@@ -19,3 +18,27 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('1000_sunny_fitness')
+
+
+# Draws pirate flag
+def draw_jolly_roger():
+    print("WELCOME TO THE 1000 SUNNY")
+    jolly_roger =  '.=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.\n'
+    jolly_roger += '|                     ______                     |\n'
+    jolly_roger += '|                  .-"      "-.                  |\n'
+    jolly_roger += '|                 /            \                 |\n'
+    jolly_roger += '|     _          |              |          _     |\n'
+    jolly_roger += '|    ( \         |,  .-.  .-.  ,|         / )    |\n'
+    jolly_roger += '|     > "=._     | )(__/  \__)( |     _.=" <     |\n'
+    jolly_roger += '|    (_/"=._"=._ |/     /\     \| _.="_.="\_)    |\n'
+    jolly_roger += '|           "=._"(_     ^^     _)"_.="           |\n'
+    jolly_roger += '|               "=\__|IIIIII|__/="               |\n'
+    jolly_roger += '|              _.="| \IIIIII/ |"=._              |\n'
+    jolly_roger += '|    _     _.="_.="\          /"=._"=._     _    |\n'
+    jolly_roger += '|   ( \_.="_.="     `--------`     "=._"=._/ )   |\n'
+    jolly_roger += '|    > _.="                            "=._ <    |\n'
+    jolly_roger += '|   (_/   free young thug                  \_)   |\n'
+    jolly_roger += '|                                                |\n'
+    jolly_roger += '.=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-.\n'
+    print(jolly_roger)
+
