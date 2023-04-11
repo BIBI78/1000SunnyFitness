5648f013907fe66f4b9194ee875bc9d57485403d
BIBI78 <blakecollins78@gmail.com>
2023-04-11 12:47:40 +0000

    import gspread etc

---
+++ run.py
@@ -0,0 +1,21 @@
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+import requests
+import json
+import random
+import time 
+from pprint import pprint
+from math import ceil
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('1000_sunny_fitness')
